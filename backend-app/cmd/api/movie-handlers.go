60fc0e527678b347c9a7c7486d981e348bec1aca
petrostrak <pit.trak@gmail.com>
2021-12-27 17:52:11 +0200

    Add DB method Get

--- backend-app/cmd/api/movie-handlers.go
+++ backend-app/cmd/api/movie-handlers.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/models"
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -21,17 +19,9 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println("id is", id)
 
-	movie := models.Movie{
-		ID:          id,
-		Title:       "Some movie",
-		Description: "Some description",
-		Year:        2021,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime:     100,
-		Rating:      5,
-		MPAARating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.logger.Println(errors.New("could not get movie"))
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, movie, "movie"); err != nil {
