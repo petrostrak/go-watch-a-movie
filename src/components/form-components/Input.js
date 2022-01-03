24456c05a6e9dea60de3e45403769a869a2b306c
petrostrak <pit.trak@gmail.com>
2022-01-03 15:22:56 +0200

    Client side validation for title field

--- src/components/form-components/Input.js
+++ src/components/form-components/Input.js
@@ -4,14 +4,17 @@ const Input = (props) => {
             <label htmlFor={props.name} className='form-label'>
             {props.title}
             </label>
-            <input 
+            <input
                 type={props.type}
-                className='form-control'
+                className={`form-control ${props.className}`}
                 id={props.name}
                 name={props.name}
                 value={props.value}
                 onChange={props.handleChange}
                 placeholder={props.placeholder}/>
+            <div className={props.errorDiv}>
+                {props.errorMsg}
+            </div>
         </div>
     );
 }
