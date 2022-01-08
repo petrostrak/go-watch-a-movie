d393fecccc6d9aef7831a25335b726eb81274267
petrostrak <pit.trak@gmail.com>
2022-01-08 19:25:47 +0200

    Handle successful login

--- src/App.js
+++ src/App.js
@@ -75,6 +75,9 @@ export default class App extends Component {
                   )}
 
                 </ul>
+                <pre>
+                  {JSON.stringify(this.state, null, 3)}
+                </pre>
               </nav>
             </div>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -70,8 +70,16 @@ export default class Login extends Component {
                     })
                 } else {
                     console.log(data);
+                    this.handleJWTChange(Object.values(data)[0]);
+                    this.props.history.push({
+                        pathname: "/admin",
+                    })
                 }
             })
+    }
+
+    handleJWTChange(jwt) {
+        this.props.handleJWTChange(jwt);
     }
 
     hasError(key) {
