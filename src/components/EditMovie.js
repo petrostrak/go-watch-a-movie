e59da4745ea54c38677c802aebbb040f971bfe70
petrostrak <pit.trak@gmail.com>
2022-01-09 15:04:56 +0200

    Add redirect to protect componets

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -88,7 +88,6 @@ export default class EditMovie extends Component {
         .then((response) => response.json())
         .then(data => {
             if (data.error) {
-              alert(data.error.message)
               this.setState({
                 alert: {type: "alert-danger",
                 message: data.error.message},
@@ -117,6 +116,14 @@ export default class EditMovie extends Component {
   }
 
 	  componentDidMount() {
+
+      if (this.props.jwt === "")  {
+        this.props.history.push({
+          pathname: "/login",
+        })
+        return;
+      }
+
 	    const id = this.props.match.params.id;
 	    if (id > 0) {
 	      fetch("http://localhost:4000/v1/movie/" + id)
