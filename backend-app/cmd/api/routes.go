877568a6746eb0f4f54b26da19f8a2e8155d3472
petrostrak <pit.trak@gmail.com>
2022-01-10 18:06:03 +0200

    Clean routes

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -27,9 +27,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
-	// router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editmovie)))
-	// router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
 
 	return app.enableCORS(router)
