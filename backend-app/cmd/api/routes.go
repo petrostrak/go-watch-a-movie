3cb84151220a3c7c22cbc42aa66802e18cddd6eb
petrostrak <pit.trak@gmail.com>
2022-01-10 18:07:49 +0200

    Add graphQL handler

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -20,15 +20,16 @@ func (app *application) routes() http.Handler {
 	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
-
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.Signin)
-
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editmovie)))
 	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
+
+	// GraphQL handler
+	router.HandlerFunc(http.MethodPost, "/v1/graphql/list", app.moviesGraphQL)
 
 	return app.enableCORS(router)
 }
