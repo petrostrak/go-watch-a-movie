6b55af76a4e270b2fc1a7e0648e7589e05c81118
petrostrak <pit.trak@gmail.com>
2022-01-10 18:02:12 +0200

    Add image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
+import "./Home.css";
+import Ticket from "./../images/movie_tickets.jpg";
 
 export default class Home extends Component {
     
     render() {
         return (
-            <h2>Welcome to Go-Movies!</h2>
+            <div className="text-center">
+                <h2>Welcome to Go-Movies!</h2>
+                <hr />
+                <img src={Ticket} alt="movie ticket" />
+                <hr />
+                <div className="tickets">
+
+                </div>
+            </div>
         );
     }
 }
